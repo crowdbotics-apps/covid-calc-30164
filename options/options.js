deada56d23b5a038c82cefd8e5de6da74e072071
Crowdbotics <team@crowdbotics.com>
2021-08-28 18:48:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "covid_calc_30164Identifier",
+  url: "https://covid_calc_30164Identifier.botics.co",
+  api: "https://covid_calc_30164Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
